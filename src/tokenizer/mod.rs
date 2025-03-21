9d2203c9be66881f74ec8b74c0dc056d84c06a44
Segfaulter <72930656+brudnevskij@users.noreply.github.com>
2025-03-21 23:15:23 +0200

    Added simple program test case to the tokenizer_tests.rs

--- src/tokenizer/mod.rs
+++ src/tokenizer/mod.rs
@@ -2,4 +2,4 @@ mod tokenizer;
 #[cfg(test)]
 mod tokenizer_tests;
 
-pub use tokenizer::Tokenizer;
+pub use tokenizer::{Tokenizer, Token, TokenType};
--- src/tokenizer/tokenizer_tests.rs
+++ src/tokenizer/tokenizer_tests.rs
@@ -1,4 +1,3 @@
-use crate::tokenizer::tokenizer::TokenType;
 use super::*;
 
 #[test]
@@ -10,4 +9,40 @@ fn test_tokenize() {
     assert_eq!(tokens[0].token_type, TokenType::Value);
     assert_eq!(tokens[1].token_type, TokenType::EOF);
     assert_eq!(tokens[0].lexeme, "value");
+}
+
+#[test]
+fn test_tokenize_simple_program() {
+    let input = "int main() { return 0; }";
+    let tokens = Tokenizer::new(input).tokenize().unwrap();
+
+    assert_eq!(tokens.len(), 10);
+    assert_eq!(tokens[0].token_type,TokenType::Value);
+    assert_eq!(tokens[0].lexeme,"int");
+
+    assert_eq!(tokens[1].token_type,TokenType::Value);
+    assert_eq!(tokens[1].lexeme,"main");
+
+    assert_eq!(tokens[2].token_type,TokenType::Punctuation);
+    assert_eq!(tokens[2].lexeme,"(");
+
+    assert_eq!(tokens[3].token_type,TokenType::Punctuation);
+    assert_eq!(tokens[3].lexeme,")");
+
+    assert_eq!(tokens[4].token_type,TokenType::Punctuation);
+    assert_eq!(tokens[4].lexeme,"{");
+
+    assert_eq!(tokens[5].token_type,TokenType::Value);
+    assert_eq!(tokens[5].lexeme,"return");
+
+    assert_eq!(tokens[6].token_type,TokenType::Value);
+    assert_eq!(tokens[6].lexeme,"0");
+
+    assert_eq!(tokens[7].token_type,TokenType::Punctuation);
+    assert_eq!(tokens[7].lexeme,";");
+
+    assert_eq!(tokens[8].token_type,TokenType::Punctuation);
+    assert_eq!(tokens[8].lexeme,"}");
+
+    assert_eq!(tokens[9].token_type,TokenType::EOF);
 }
